f29d433984b4ddc19f02168e73126724ff2f5759
alshayna <katarinablue1420@gmail.com>
2026-05-14 16:09:21 +0700

    Add files via upload

---
+++ belajar.php
@@ -0,0 +1,29 @@
+<?php
+
+echo "Hello World";
+
+echo "\n";
+
+$nama = "alshayna";
+echo $nama;
+
+echo "\n";
+
+$umur = 17;
+echo $umur;
+
+echo "\n";
+
+if ($umur >= 17) {
+    echo "Sudah dewasa";
+} else {
+    echo "Masih anak-anak";
+}
+
+echo "\n";
+
+for ($i = 1; $i <= 3; $i++) {
+    echo "Belajar PHP\n";
+}
+
+?>
